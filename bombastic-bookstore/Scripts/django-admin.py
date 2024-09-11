d36bc6b62362eb047d37bc0bd2354ab688d6b266
Brandon Biggs <brandonbiggs15@gmail.com>
2024-09-11 00:48:17 -0400

    Django mysite project creation
    Created:
    - directory
    - setup virtual environment
    - installed Django
    - started "mysite" project
    - executes "python manage.py runserver 0.0.0.0:8000" command with no issues.

---
+++ bombastic-bookstore/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Valued Customer\Documents\Bombastic Bookstore Project\bombastic-bookstore\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
