f68bb79b90ad1a0d4377cba235c6c7e1cd5ce1c7
Ryan Burres <rsburres@my.waketech.edu>
2024-09-21 14:37:35 -0400

    Created a home template with search bar to search through books
    The search uses Django forms to search using isbn, title, and author. The search bar is very user friendly.

--- BombasticBookstore/bombasticbookstore/book_app/models.py
+++ BombasticBookstore/bombasticbookstore/book_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    isbn = models.CharField(max_length=13, default="0000000000000")
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
--- BombasticBookstore/bombasticbookstore/book_app/views.py
+++ BombasticBookstore/bombasticbookstore/book_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import FormView, ListView
+from .forms import SearchForm
+from .models import Book
+
 
 # Create your views here.
+class HomepageView(FormView):
+    template_name = "home.html"
+    form_class = SearchForm
+
+
+class SearchResultsView(ListView):
+    model = Book
+    template_name = "search_results.html"
+
+    def get_queryset(self):
+        query = self.request.GET["q"]
+        object_list = (
+            Book.objects.filter(title__icontains=query)
+            | Book.objects.filter(author__icontains=query)
+            | Book.objects.filter(isbn__icontains=query)
+        )
+        return object_list
