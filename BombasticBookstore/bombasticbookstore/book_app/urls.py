0e8b223be90a9172bd74daec27e1f236b13cf4d3
jbm <108535929+wolfpack83@users.noreply.github.com>
2024-10-09 13:29:01 -0400

    add view all and edit pages

--- BombasticBookstore/bombasticbookstore/book_app/urls.py
+++ BombasticBookstore/bombasticbookstore/book_app/urls.py
@@ -5,6 +5,9 @@ from .views import SearchResultsView, CustomLoginView, SearchView, HomepageView
 urlpatterns = [
     path("", HomepageView.as_view(), name="index"),
     path('add_book/', views.add_book, name='add_book'),
+    path('adjust_qty/<int:id>', views.adjust_qty, name='adjust_qty'),
+    path('adjust_qty/updaterecord/<int:id>', views.update_record, name='update_record'),
     path("search/", SearchView.as_view(), name="search"),
     path("search-results/", SearchResultsView.as_view(), name="search_results"),
+    path('view_all/', views.view_all, name='view_all')
 ]
