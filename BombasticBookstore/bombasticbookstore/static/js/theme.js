bf72e91efed696b0e17222350c8cebe35dc8fe42
Jay-Chavis-1738 <jaychavis17@gmail.com>
2024-11-07 23:39:40 -0500

    update css

--- BombasticBookstore/bombasticbookstore/static/js/theme.js
+++ BombasticBookstore/bombasticbookstore/static/js/theme.js
@@ -1,13 +1,49 @@
 document.addEventListener('DOMContentLoaded', function() {
     const themeSelect = document.getElementById('theme-select');
-    const currentTheme = document.body.className;
+    const currentTheme = getCookie('theme') || 'light';
 
+    document.body.className = currentTheme;
     themeSelect.value = currentTheme;
+
+    applyTheme(currentTheme);
 
     themeSelect.addEventListener('change', function() {
         const selectedTheme = themeSelect.value;
         document.body.className = selectedTheme;
-        document.getElementById('theme-link').href = `/static/css/${selectedTheme}.css`;
-        document.cookie = `theme=${selectedTheme}; path=/;`;
+        setCookie('theme', selectedTheme, 365);
+        applyTheme(selectedTheme);
     });
 });
+
+function applyTheme(theme) {
+    const elementsToTheme = document.querySelectorAll('.theme-dependent, .search-card');
+
+    elementsToTheme.forEach(element => {
+        if (theme === 'dark') {
+            element.classList.add('dark');
+        } else {
+            element.classList.remove('dark');
+        }
+    });
+}
+
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
